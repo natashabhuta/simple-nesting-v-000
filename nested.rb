5b331960ac402b4704cb7073500b71f0854f29fa
natashabhuta <natasha.bhuta@gmail.com>
2018-08-13 16:55:11 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -66,10 +66,10 @@ def adding_matz
 # add the following information to the top level of programmer_hash
 # :yukihiro_matsumoto => {
 #   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
+#    :languages => ["LISP, C"]
 # }
 
-	programmer_hash = 
+	programmer_hash =
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -85,15 +85,13 @@ def adding_matz
         }
      }
 
-    programmer_hash = 
-    :yukihiro_matsumoto => {
-      :known_for => "Ruby", 
-      :languages => ["LISP", "C"]}
-    programmer_hash
+programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
+programmer_hash
+
 end
 
 def changing_alan
-	programmer_hash = 
+	programmer_hash =
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -108,14 +106,14 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
+     #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
-     
-     
+		 programmer_hash[:alan_kay][:known_for] = alans_new_info
+		 programmer_hash
 end
 
 def adding_to_dennis
-	programmer_hash = 
+	programmer_hash =
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -131,4 +129,6 @@ def adding_to_dennis
         }
      }
 
+		 programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+		 programmer_hash
 end
